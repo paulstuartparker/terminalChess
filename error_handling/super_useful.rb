090c27cc4f56abba5572feb85f3181112a154fcf
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-09-11 13:44:54 -0400

    finish error handling exercise

--- error_handling/super_useful.rb
+++ error_handling/super_useful.rb
@@ -1,17 +1,26 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+rescue ArgumentError => error
+  puts error.message
+  return nil
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
+class CoffeeError < StandardError
+end
+
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  elsif maybe_fruit == "coffee" || maybe_fruit == "Coffee"
+    raise CoffeeError.new("Try Again, I love coffee")
+
+  else
+    raise StandardError
+  end
 end
 
 def feed_me_a_fruit
@@ -19,12 +28,30 @@ def feed_me_a_fruit
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+  reaction(maybe_fruit)
+rescue CoffeeError => error
+  puts error.message
+  retry
+rescue StandardError => error
+  puts "That's not a fruit"
+
+
+end
 
 # PHASE 4
+class FriendshipLengthError < StandardError
+end
+class NameLengthError < StandardError
+end
+class PasttimeError < StandardError
+end
+
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise FriendshipLengthError.new("Maybe later...") if yrs_known < 5
+    raise NameLengthError.new("you need a name") if name.empty?
+    raise PasttimeError.new("pasttime must be something") if fav_pastime.empty?
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
@@ -39,8 +66,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
--- error_handling/user_script.rb
+++ error_handling/user_script.rb
@@ -1,10 +1,10 @@
 require_relative 'super_useful'
+#
+# puts "'five' == #{convert_to_int('five')}"
+#
+# feed_me_a_fruit
 
-puts "'five' == #{convert_to_int('five')}"
-
-feed_me_a_fruit
-
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('sam', 5, 'baseball')
 
 sam.talk_about_friendship
 sam.do_friendstuff
